4171a7d5552934efab8e09fef4267669044da6dd
PRM710 <99472825+PRM710@users.noreply.github.com>
2025-01-16 21:28:11 +0530

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'https://backend-for-deployment.onrender.com' : '/';
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000' // Local development URL
+    : 'https://backend-for-deployment.onrender.com'; // Your Render backend URL for production
